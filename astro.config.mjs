8fdc887f74e9d81252f4eb2e34b18cca019c669c
kuhjuice <bo.kern@posteo.de>
2023-10-10 17:28:10 +0200

    no base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kuhjuice.github.io',
-  base: '/AstroPortfolioWebsite',
   integrations: [tailwind()]
-  
+
 });
